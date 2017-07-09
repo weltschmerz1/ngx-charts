669488dab67df45930e6642dc6a457da536edbe6
Dennis C Hackethal <dennis.hackethal@gmail.com>
2017-07-09 01:14:19 -0700

    Package

--- release/index.js
+++ release/index.js
@@ -2414,6 +2414,145 @@ module.exports = Array.isArray || function (arr) {
 
 /***/ }),
 
+/***/ "./node_modules/simplify-js/simplify.js":
+/***/ (function(module, exports, __webpack_require__) {
+
+var __WEBPACK_AMD_DEFINE_RESULT__;/*
+ (c) 2013, Vladimir Agafonkin
+ Simplify.js, a high-performance JS polyline simplification library
+ mourner.github.io/simplify-js
+*/
+
+(function () { 'use strict';
+
+// to suit your point format, run search/replace for '.x' and '.y';
+// for 3D version, see 3d branch (configurability would draw significant performance overhead)
+
+// square distance between 2 points
+function getSqDist(p1, p2) {
+
+    var dx = p1.x - p2.x,
+        dy = p1.y - p2.y;
+
+    return dx * dx + dy * dy;
+}
+
+// square distance from a point to a segment
+function getSqSegDist(p, p1, p2) {
+
+    var x = p1.x,
+        y = p1.y,
+        dx = p2.x - x,
+        dy = p2.y - y;
+
+    if (dx !== 0 || dy !== 0) {
+
+        var t = ((p.x - x) * dx + (p.y - y) * dy) / (dx * dx + dy * dy);
+
+        if (t > 1) {
+            x = p2.x;
+            y = p2.y;
+
+        } else if (t > 0) {
+            x += dx * t;
+            y += dy * t;
+        }
+    }
+
+    dx = p.x - x;
+    dy = p.y - y;
+
+    return dx * dx + dy * dy;
+}
+// rest of the code doesn't care about point format
+
+// basic distance-based simplification
+function simplifyRadialDist(points, sqTolerance) {
+
+    var prevPoint = points[0],
+        newPoints = [prevPoint],
+        point;
+
+    for (var i = 1, len = points.length; i < len; i++) {
+        point = points[i];
+
+        if (getSqDist(point, prevPoint) > sqTolerance) {
+            newPoints.push(point);
+            prevPoint = point;
+        }
+    }
+
+    if (prevPoint !== point) newPoints.push(point);
+
+    return newPoints;
+}
+
+// simplification using optimized Douglas-Peucker algorithm with recursion elimination
+function simplifyDouglasPeucker(points, sqTolerance) {
+
+    var len = points.length,
+        MarkerArray = typeof Uint8Array !== 'undefined' ? Uint8Array : Array,
+        markers = new MarkerArray(len),
+        first = 0,
+        last = len - 1,
+        stack = [],
+        newPoints = [],
+        i, maxSqDist, sqDist, index;
+
+    markers[first] = markers[last] = 1;
+
+    while (last) {
+
+        maxSqDist = 0;
+
+        for (i = first + 1; i < last; i++) {
+            sqDist = getSqSegDist(points[i], points[first], points[last]);
+
+            if (sqDist > maxSqDist) {
+                index = i;
+                maxSqDist = sqDist;
+            }
+        }
+
+        if (maxSqDist > sqTolerance) {
+            markers[index] = 1;
+            stack.push(first, index, index, last);
+        }
+
+        last = stack.pop();
+        first = stack.pop();
+    }
+
+    for (i = 0; i < len; i++) {
+        if (markers[i]) newPoints.push(points[i]);
+    }
+
+    return newPoints;
+}
+
+// both algorithms combined for awesome performance
+function simplify(points, tolerance, highestQuality) {
+
+    var sqTolerance = tolerance !== undefined ? tolerance * tolerance : 1;
+
+    points = highestQuality ? points : simplifyRadialDist(points, sqTolerance);
+    points = simplifyDouglasPeucker(points, sqTolerance);
+
+    return points;
+}
+
+// export as AMD module / Node module / browser or worker variable
+if (true) !(__WEBPACK_AMD_DEFINE_RESULT__ = function() { return simplify; }.call(exports, __webpack_require__, exports, module),
+				__WEBPACK_AMD_DEFINE_RESULT__ !== undefined && (module.exports = __WEBPACK_AMD_DEFINE_RESULT__));
+else if (typeof module !== 'undefined') module.exports = simplify;
+else if (typeof self !== 'undefined') self.simplify = simplify;
+else window.simplify = simplify;
+
+})();
+
+
+/***/ }),
+
 /***/ "./node_modules/webpack/buildin/global.js":
 /***/ (function(module, exports) {
 
@@ -15617,6 +15756,10 @@ LineSeriesComponent = __decorate([
 /* harmony import */ var __WEBPACK_IMPORTED_MODULE_1__angular_animations___default = __webpack_require__.n(__WEBPACK_IMPORTED_MODULE_1__angular_animations__);
 /* harmony import */ var __WEBPACK_IMPORTED_MODULE_2_d3_selection__ = __webpack_require__(5);
 /* harmony import */ var __WEBPACK_IMPORTED_MODULE_2_d3_selection___default = __webpack_require__.n(__WEBPACK_IMPORTED_MODULE_2_d3_selection__);
+/* harmony import */ var __WEBPACK_IMPORTED_MODULE_3_d3_shape__ = __webpack_require__(4);
+/* harmony import */ var __WEBPACK_IMPORTED_MODULE_3_d3_shape___default = __webpack_require__.n(__WEBPACK_IMPORTED_MODULE_3_d3_shape__);
+/* harmony import */ var __WEBPACK_IMPORTED_MODULE_4_simplify_js__ = __webpack_require__("./node_modules/simplify-js/simplify.js");
+/* harmony import */ var __WEBPACK_IMPORTED_MODULE_4_simplify_js___default = __webpack_require__.n(__WEBPACK_IMPORTED_MODULE_4_simplify_js__);
 var __decorate = (this && this.__decorate) || function (decorators, target, key, desc) {
     var c = arguments.length, r = c < 3 ? target : desc === null ? desc = Object.getOwnPropertyDescriptor(target, key) : desc, d;
     if (typeof Reflect === "object" && typeof Reflect.decorate === "function") r = Reflect.decorate(decorators, target, key, desc);
@@ -15626,6 +15769,8 @@ var __decorate = (this && this.__decorate) || function (decorators, target, key,
 var __metadata = (this && this.__metadata) || function (k, v) {
     if (typeof Reflect === "object" && typeof Reflect.metadata === "function") return Reflect.metadata(k, v);
 };
+
+
 
 
 
@@ -15647,8 +15792,30 @@ var LineComponent = (function () {
     };
     LineComponent.prototype.animateToCurrentForm = function () {
         var node = __webpack_require__.i(__WEBPACK_IMPORTED_MODULE_2_d3_selection__["select"])(this.element.nativeElement).select('.line');
+        var commands = this.path.split(/(?=[LMC])/);
+        var coordinates = commands.map(function (d) {
+            var pointsArray = d.slice(1, d.length).split(',');
+            // let pairsArray = [];
+            // for(let i = 0; i < pointsArray.length; i += 2){
+            //   pairsArray.push({
+            //     x: +pointsArray[i],
+            //     y: +pointsArray[i+1]
+            //   });
+            // }
+            // return pairsArray[0];
+            return {
+                x: pointsArray[0],
+                y: pointsArray[1]
+            };
+        });
+        var simplifiedCoordinates = __WEBPACK_IMPORTED_MODULE_4_simplify_js__(coordinates, 5);
+        // console.log(coordinates);
+        var linefn = __webpack_require__.i(__WEBPACK_IMPORTED_MODULE_3_d3_shape__["line"])()
+            .x(function (d) { return d.x; })
+            .y(function (d) { return d.y; });
+        var simplifiedPath = linefn(simplifiedCoordinates);
         node
-            .attr('d', this.path);
+            .attr('d', simplifiedPath);
     };
     return LineComponent;
 }());
--- release/line-chart/line.component.js
+++ release/line-chart/line.component.js
@@ -1,6 +1,8 @@
 import { Component, Input, Output, EventEmitter, ElementRef, ChangeDetectionStrategy } from '@angular/core';
 import { trigger, style, animate, transition } from '@angular/animations';
 import { select } from 'd3-selection';
+import { line } from 'd3-shape';
+import * as simplify from 'simplify-js';
 var LineComponent = (function () {
     function LineComponent(element) {
         this.element = element;
@@ -19,8 +21,30 @@ var LineComponent = (function () {
     };
     LineComponent.prototype.animateToCurrentForm = function () {
         var node = select(this.element.nativeElement).select('.line');
+        var commands = this.path.split(/(?=[LMC])/);
+        var coordinates = commands.map(function (d) {
+            var pointsArray = d.slice(1, d.length).split(',');
+            // let pairsArray = [];
+            // for(let i = 0; i < pointsArray.length; i += 2){
+            //   pairsArray.push({
+            //     x: +pointsArray[i],
+            //     y: +pointsArray[i+1]
+            //   });
+            // }
+            // return pairsArray[0];
+            return {
+                x: pointsArray[0],
+                y: pointsArray[1]
+            };
+        });
+        var simplifiedCoordinates = simplify(coordinates, 5);
+        // console.log(coordinates);
+        var linefn = line()
+            .x(function (d) { return d.x; })
+            .y(function (d) { return d.y; });
+        var simplifiedPath = linefn(simplifiedCoordinates);
         node
-            .attr('d', this.path);
+            .attr('d', simplifiedPath);
     };
     return LineComponent;
 }());
